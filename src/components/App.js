4087d82bd1fc8a155f6c73a0de1355e4f3b8418e
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-05 15:02:26 +0800

    finishing the style of the home hero

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import Header from './elements/Header.js';
 import Home from './Home.js';
 
--- src/components/elements/HeroImage.js
+++ src/components/elements/HeroImage.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-const HeroImage = () => {
+import { StyledHeroImage } from '../styles/StyledHeroImage'
+
+const HeroImage = ({image, title, text}) => {
   return (
-    <div>
-      Hero Image
-    </div>
+    <StyledHeroImage image={image}>
+      <div className="heroimage-content">
+        <div className="heroimage-text">
+          <h1>{title}</h1>
+          <p>{text}</p>
+        </div>
+      </div>
+    </StyledHeroImage>
   )
 }
 
