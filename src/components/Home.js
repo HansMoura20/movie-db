c1ac4247c37a9adc55b4fdf80221891831aa3ada
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-05 16:39:19 +0800

    retrieve all the movie thumbnails and titles than pass it to the grid.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { 
   API_URL, 
   API_KEY, 
@@ -18,8 +18,11 @@ import Spinner from './elements/Spinner';
 //import custom hook
 import { useHomeFetch } from './hooks/useHomeFetch';
 
+import NoImage from './images/no_image.jpg'
+
 const Home =() =>{
 const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+const [searchTerm, setSearchTerm] = useState('');
 console.log(state);
 
 if(error) return <div>Something went wrong</div>
@@ -33,7 +36,21 @@ if(!state.movies[0]) return <Spinner/>
         text={state.heroImage.overview}
       />
       <SearchBar/>
-      <Grid/>
+      <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+        {state.movies.map(movie => (
+          <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path 
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : NoImage 
+              }
+              movieId={movie.id}
+              movieName={movie.original_title}
+          />
+        ))}
+      </Grid>
       <MovieThumb/>
       <Spinner/>
       <LoadMoreBtn/>
